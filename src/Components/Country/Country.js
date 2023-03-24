dcbce7b778c9de8af21d660e45c76f9be384be02
Mustakim <sycospike1@gmail.com>
2023-03-24 15:53:09 +0600

    added first react project

---
+++ src/Components/Country/Country.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Country.css';
+
+const Country = (props) => {
+    const {area,region ,population,name , flags} = props.country ;
+    return (
+        <div className='country'>
+            <h2>Country Name :{name.common} </h2>
+            <img src={flags.png} alt="" />
+            <p>Population : {population}</p>
+            <p>Region : {region}</p>
+            <p><small>Area : {area}</small></p>
+        </div>
+    );
+};
+
+export default Country;
